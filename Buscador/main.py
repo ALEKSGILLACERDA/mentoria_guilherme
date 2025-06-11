e4c75dd8bea70fd837a89d63fb77b3580a5bbb53
aleks gil carvalho <aleksgillacerda@gmail.com>
2025-06-11 20:48:48 -0300

    ajustes para deixar o código mais limpo

--- Buscador/main.py
+++ Buscador/main.py
@@ -1,19 +1,17 @@
 from gupy_api import coletar_todas_as_vagas
 from gupy_api import salvar_vagas_em_arquivo
-from typing import List, Dict, Any, Optional
+from typing import List, Optional
 
-def main(termos_de_busca: Optional[List[str]] = None, offset_final: int = 50) -> None:
+def main(termos_de_busca: Optional[List[str]] = None, offset_final: int = 640) -> None:
     """função principal que define os termos padrão e o limite de páginas, se não for passado nenhum parâmetro externo"""
     if termos_de_busca is None:
         termos_de_busca = [
             "dados"
         ]
-    offsets: range = range(0, 640, 10)
+    offsets = range(0, 640, 10)
 
-    todas_as_vagas: List[Dict[str, Any]] = coletar_todas_as_vagas(termos_de_busca, offsets)
+    todas_as_vagas = coletar_todas_as_vagas(termos_de_busca, offsets)
     salvar_vagas_em_arquivo(todas_as_vagas, "vagas_gupy.json")
 
 if __name__ == "__main__":
     main()
-
-main()
