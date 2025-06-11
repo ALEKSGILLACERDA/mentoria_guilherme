beb93aea56bd8cc930bc4e8b4979b56c73c1993d
aleks gil carvalho <aleksgillacerda@gmail.com>
2025-06-11 20:30:12 -0300

    colocando o Main somente no arquivo main

--- Buscador/main.py
+++ Buscador/main.py
@@ -1,5 +1,19 @@
-from gupy_api import main
+from gupy_api import coletar_todas_as_vagas
+from gupy_api import salvar_vagas_em_arquivo
+from typing import List, Dict, Any, Optional
 
-total_vagas = 640  # quer buscar até N vagas
+def main(termos_de_busca: Optional[List[str]] = None, offset_final: int = 50) -> None:
+    """função principal que define os termos padrão e o limite de páginas, se não for passado nenhum parâmetro externo"""
+    if termos_de_busca is None:
+        termos_de_busca = [
+            "dados"
+        ]
+    offsets: range = range(0, 640, 10)
 
-main(offset_final=total_vagas)
+    todas_as_vagas: List[Dict[str, Any]] = coletar_todas_as_vagas(termos_de_busca, offsets)
+    salvar_vagas_em_arquivo(todas_as_vagas, "vagas_gupy.json")
+
+if __name__ == "__main__":
+    main()
+
+main()
